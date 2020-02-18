cf49886ab8a7117b5e6cbee763c8809e6301a5b8
Sameeksha Katoch <skatoch1@asu.edu>
2020-02-18 15:35:12 -0700

    Minor changes

--- hyper_tuning_main.py
+++ hyper_tuning_main.py
@@ -20,7 +20,7 @@ from sklearn import metrics
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 from xgboost import XGBClassifier
-
+import matplotlib.pyplot as plt
 
 training_df = pd.read_csv('nan_empty_allzero.csv')
 features = training_df.columns
@@ -55,9 +55,9 @@ param_grid = [
     'classifier__C' : np.logspace(-4, 4, 20), #Inverse of regularization strength
     'classifier__solver' : ['liblinear']},
     {'classifier' : [RandomForestClassifier()],
-    'classifier__n_estimators' : list(range(10,101,10)),
+    'classifier__n_estimators' : list(range(10,101,20)),
     'classifier__criterion': ['entropy', 'gini'],
-    'classifier__max_features' : list(range(10,len(features),20))},
+    'classifier__max_features' : list(range(10,len(features),30))},
     {'classifier' : [XGBClassifier()],
     'classifier__objective' : ['reg:logistic'],
     'classifier__learning_rate': list(np.arange(0.,0.1,0.05)),
@@ -67,11 +67,14 @@ param_grid = [
 
 # Create grid search object
 
-clf = GridSearchCV(pipe, param_grid = param_grid, cv = 5, verbose=True, n_jobs=-1)
+clf = GridSearchCV(pipe, param_grid = param_grid, cv = 3, verbose=True, n_jobs=4)
 
 # Fit on data
-
+        
 best_clf = clf.fit(X_train, y_train)
 y_pred = best_clf.predict(X_test)
 print("AUC Score", metrics.roc_auc_score(y_test.values, clf.predict_proba(X_test)[:,1]))
 metrics.plot_confusion_matrix(clf, X_test, y_test)
+#optimised_random_forest = clf.best_estimator_
+joblib_file = os.path.join('Random_Forest',".pkl")
+joblib.dump(best_clf, joblib_file)
